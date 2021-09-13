45036a2fc64f8e8b31c87765c956db6ad296e050
João Paulo <jp_romana@outlook.com>
2021-09-12 22:19:12 -0300

    Jpa repository, injeção de dependencia, implemantação no banco de dados

---
+++ src/main/java/com/romanojp/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.romanojp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.romanojp.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
