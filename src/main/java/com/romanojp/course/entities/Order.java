4427f5edf76a66156145c6fece929f0f8bce8d7e
João Paulo <jp_romana@outlook.com>
2021-09-19 20:14:55 -0300

    metodos subtotal e total

--- src/main/java/com/romanojp/course/entities/Order.java
+++ src/main/java/com/romanojp/course/entities/Order.java
@@ -124,6 +124,14 @@ public class Order implements Serializable {
 	public Set<OrderItem>getItems(){
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.00;
+		for(OrderItem x : items) {
+			sum += x.getSubtotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
